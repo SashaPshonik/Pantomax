5172f4b2db644508af747d236a669caf7d5b2207
Sasha <sasha.pshonsk@gmail.com>
2018-03-22 11:48:54 +0200

    Pleasure section

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,7 +1,7 @@
 module.exports = function () {
     $.gulp.task('watch', function () {
         $.gulp.watch('./dev/**/*.pug', $.gulp.series('pug'));
-        $.gulp.watch('./dev/stylus/**/*question-section.styl', $.gulp.series('styles:dev'));
+        $.gulp.watch('./dev/stylus/**/*.styl', $.gulp.series('styles:dev'));
         $.gulp.watch('./dev/img/svg/*.svg', $.gulp.series('svg'));
         $.gulp.watch('./dev/js/**/*.js', $.gulp.series('libsJS:dev', 'js:copy'));
         $.gulp.watch(['./dev/img/**/*.{png,jpg,gif}',
